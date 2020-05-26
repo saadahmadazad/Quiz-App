dd0a67296dc3d610a30d3d387442813701548a54
Muhammad Arshad <arshadaidev@gmail.com>
2020-05-26 05:15:02 -0700

    Quiz Application Updated

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const profileRoutes = require("./src/routes/profile-routes");
 const quizRoutes = require("./src/routes/quiz-routes");
 const areaRoutes = require("./src/routes/area-routes");
 const keyRoutes = require("./src/routes/key-routes");
+const path = require('path');
 require("./src/db/mongoose");
 
 app.use(express.json());
@@ -16,3 +17,12 @@ const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`API listening on port ${port}...`);
 });
+
+if(process.env.NODE_ENV === 'production') {
+	
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+}
