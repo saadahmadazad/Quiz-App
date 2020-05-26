1b1b360b8b591604402b906275e4bfa6b5a1673d
Muhammad Arshad <arshadaidev@gmail.com>
2020-05-26 05:52:11 -0700

    ne update

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.listen(port, () => {
 
 if(process.env.NODE_ENV === 'production') {
 	
-	app.use(express.static('client/build'));
+	app.use(express.static('client/public'));
 
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
